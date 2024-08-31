a50e3f03d0f17401b34eebd76a274b163e2a0e9a
courfedra <franandres93@gmail.com>
2024-08-31 10:05:36 -0300

    structure footer Link Logo

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -1,7 +1,30 @@
+import "./footer.css";
+import { Link } from "react-router-dom";
+import {Logo} from "../header/Logo"
+import { NavbarMenu } from "../RoutesConfig";
+
 export const Footer =()=>{
     return(
         <footer>
-            Footer de prueba
+            <div className="logoFooter">
+                <Logo/>
+            </div>
+            <div>
+                <ul className="ulFooter">
+                {NavbarMenu.map((e)=>{
+                    return(
+                        <Link
+                        key={NavbarMenu.indexOf(e)}
+                        to={e.path}>
+                            {e.name}
+                        </Link>
+                    )
+                    })}
+                </ul>
+            </div>
+            <div>
+                redes
+            </div>
         </footer>
     )
 }
--- src/header/Logo.jsx
+++ src/header/Logo.jsx
@@ -1,8 +1,11 @@
 import logoDede from "../assets/DedeBeautyLogo24.png"
+import { Link } from "react-router-dom"
 export const Logo =()=>{
     return(
         <div>
-            <img src={logoDede} alt="Logo DeDe" style={{width:"125px"}}/>
+            <Link to="/">
+                <img src={logoDede} alt="Logo DeDe" style={{width:"125px"}}/>
+            </Link>
         </div>
     )
 }
