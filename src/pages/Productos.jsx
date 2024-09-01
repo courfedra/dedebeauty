0013da37860e763b7c59e742e42229a82d36c5c3
courfedra <franandres93@gmail.com>
2024-09-01 09:29:59 -0300

    init product math and design structure

--- src/pages/Productos.jsx
+++ src/pages/Productos.jsx
@@ -1,5 +1,44 @@
+import productos from "../assets/products/products.json"
+import "./productos.css"
 export const Productos = () => {
+    function porcentaje (precioOriginal,descuento){
+        let auxiliar = precioOriginal-((descuento*precioOriginal)/100);
+
+        return auxiliar
+    }
     return(
-        <h1 style={{height:'100vh'}}> Productos </h1>
+        <section className="productos">
+            {productos.map((e)=>{
+                return(
+                    <div className="card" key={productos.indexOf(e)}>
+                        <div className="card-top">
+                            <img src={e.foto} alt={e.marca}/>
+                            <div className="card-top_brand-name">
+                                <p>{e.marca}</p>
+                                <p>{e.nombre}</p>
+                            </div>
+                        </div>
+                        <div className="card-description">
+                            <p>{e.descripcion}</p>
+                        </div>
+                        <div className="card-price">
+                            {e.descuento.hayDescuento
+                                ?<div>
+                                    <p className="precio-tachado">
+                                        {e.precio}
+                                    </p>
+                                    <p className="precio-descuento">
+                                        {porcentaje(e.precio,e.descuento.totalDescuento)}
+                                    </p>
+                                </div>
+                                :<p>{e.precio}</p>}
+                        </div>
+                        <p>{e.stock}</p>
+                    </div>
+                )
+            })
+            }
+        </section>
     )
 }
+
